6690e1c4a4b670083b2402d86d0dd40b11761b5a
Veronica_Lapunka <Veronica_Lapunka@epam.com>
2022-04-26 12:26:19 +0300

    Deleted unused method.

--- pythonFW/tests/base/base_test.py
+++ pythonFW/tests/base/base_test.py
@@ -13,12 +13,6 @@ class BaseTest:
     def teardown(self):
         Logger.get_instance().write_log_to_file()
 
-    @staticmethod
-    def get_header(response: Response, header_name):
-        if header_name in response.headers:
-            return {header_name: response.headers[header_name]}
-        else:
-            raise Exception(f"Cannot find header with the name {header_name} in the last response")
 
     @staticmethod
     def create_unique_email(base: str, domain="gmail.com"):
